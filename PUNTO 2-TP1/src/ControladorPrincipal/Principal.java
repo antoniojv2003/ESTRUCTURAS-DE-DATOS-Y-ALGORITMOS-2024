03e624d4da9b97369541f8fbbc6765d4d0f84005
antoniojv2003 <ajv7070@gmail.com>
2024-04-01 10:00:37 -0300

    inicio del punto-creación del main

---
+++ PUNTO 2-TP1/src/ControladorPrincipal/Principal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ControladorPrincipal;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Principal { //clase con el método main
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
