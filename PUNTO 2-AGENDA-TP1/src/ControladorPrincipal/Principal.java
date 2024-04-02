dcdca9720e2ef2d5d32c5c269443cee748a46a4c
antoniojv2003 <ajv7070@gmail.com>
2024-04-01 22:59:29 -0300

    Comienzo del TP1-PUNTO2-AGENDA

---
+++ PUNTO 2-AGENDA-TP1/src/ControladorPrincipal/Principal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ControladorPrincipal;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Principal { //clase donde estará el método main
+    public static void main(String[] args) {
+
+        
+    }
+    
+}
